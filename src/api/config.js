d3cf848a6882f24d8308d2d46ca21e25745a5688
Daniela Munoz <daniela.munoz@enova.mx>
2018-01-23 18:41:36 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+/* Application name	Platzimusic
+API key	054738b6331cfc20a86bda20383c19e0
+Shared secret	a66a973119c72db473aa640a85918f09
+Registered to	dnnycortes */
+
+export default {
+    appname: 'Platzimusic',
+    apiKey: '054738b6331cfc20a86bda20383c19e0',
+    secret: 'a66a973119c72db473aa640a85918f09',
+    registeredTo: 'dnnycortes'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    li 
+        h2: a( v-bind:href="artist.url" target="_blank" ) {{ artist.name }}
+        img( v-bind:src="artist.image[2]['#text']" )
+</template>
+
+<script>
+export default {
+  name: 'artist', // nombre del componente
+  props: ['artist'] // propiedades que recibe
+}
+</script>
+
+<style lang="stylus" scoped>
+li 
+    display block 
+    margin 10px 0
+</style>
